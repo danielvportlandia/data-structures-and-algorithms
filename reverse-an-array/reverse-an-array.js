8a05f6f581b3687c62c378d42eb5bd390dd6438c
danielvportlandia <danielshelton90@gmail.com>
2018-04-17 06:35:37 -0700

    linter errors fixed.

--- reverse-an-array/reverse-an-array.js
+++ reverse-an-array/reverse-an-array.js
@@ -1,7 +1,9 @@
-reverseArray = () => {
-    let reversedArr = [];
-    for (let i = arr.length - 1; i >= 0; i--) {
-      reversedArr.push(arr[i])
-    }
-    return reversedArr;
+function reverseArray(arr) {
+  const reversedArr = [];
+  for (let i = arr.length - 1; i >= 0; i - 1) {
+    reversedArr.push(arr[i]);
   }
+  return reversedArr;
+}
+
+reverseArray();
